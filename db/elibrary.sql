069b575a67db5dded5701be1b400b724c85b67f7
greenmr7 <greenmr791@gmail.com>
2019-05-17 06:43:26 +0700

    (-)update database

--- db/elibrary.sql
+++ db/elibrary.sql
@@ -19,82 +19,29 @@ USE `elibrary`;
 -- Dumping structure for table elibrary.anggota
 CREATE TABLE IF NOT EXISTS `anggota` (
   `id_anggota` int(11) NOT NULL AUTO_INCREMENT,
-  `id_user` int(11) NOT NULL,
   `status_anggota` enum('Active','Non-Active','','') CHARACTER SET utf8 NOT NULL,
   `nama_anggota` varchar(50) CHARACTER SET utf8 NOT NULL,
-  `email` varchar(255) CHARACTER SET utf8 NOT NULL,
   `tlp` varchar(50) CHARACTER SET utf8 NOT NULL,
-  `instansi` varchar(50) CHARACTER SET utf8 NOT NULL,
-  `username` varchar(32) CHARACTER SET utf8 NOT NULL,
-  `password` varchar(64) CHARACTER SET utf8 NOT NULL,
   `tanggal` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id_anggota`),
-  UNIQUE KEY `username` (`username`)
+  PRIMARY KEY (`id_anggota`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 -- Dumping data for table elibrary.anggota: ~2 rows (approximately)
 /*!40000 ALTER TABLE `anggota` DISABLE KEYS */;
-INSERT INTO `anggota` (`id_anggota`, `id_user`, `status_anggota`, `nama_anggota`, `email`, `tlp`, `instansi`, `username`, `password`, `tanggal`) VALUES
-	(2, 4, 'Active', 'picollococo', 'pico@gmas.com', '87897894654213', '   wakwaw ', 'pco', 'f7c3bc1d808e04732adf679965ccc34ca7ae3441', '2019-03-18 00:03:31'),
-	(3, 4, 'Active', 'miasda', 'nmasd@gmail.com', '654564', 'uweeesss', 'mimi', 'f7c3bc1d808e04732adf679965ccc34ca7ae3441', '2019-03-18 00:07:30');
+INSERT INTO `anggota` (`id_anggota`, `status_anggota`, `nama_anggota`, `tlp`, `tanggal`) VALUES
+	(2, 'Active', 'picollococo', '87897894654213', '2019-03-18 00:03:31'),
+	(3, 'Active', 'miasda', '654564', '2019-03-18 00:07:30');
 /*!40000 ALTER TABLE `anggota` ENABLE KEYS */;
-
--- Dumping structure for table elibrary.bahasa
-CREATE TABLE IF NOT EXISTS `bahasa` (
-  `id_bahasa` int(11) NOT NULL AUTO_INCREMENT,
-  `kode_bahasa` varchar(3) CHARACTER SET utf8 NOT NULL,
-  `nama_bahasa` varchar(50) CHARACTER SET utf8 NOT NULL,
-  `keterangan` text,
-  `urutan` int(11) DEFAULT NULL,
-  `tanggal` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id_bahasa`),
-  UNIQUE KEY `kode_bhs` (`kode_bahasa`),
-  UNIQUE KEY `nama_bhs` (`nama_bahasa`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-
--- Dumping data for table elibrary.bahasa: ~1 rows (approximately)
-/*!40000 ALTER TABLE `bahasa` DISABLE KEYS */;
-INSERT INTO `bahasa` (`id_bahasa`, `kode_bahasa`, `nama_bahasa`, `keterangan`, `urutan`, `tanggal`) VALUES
-	(1, 'K01', 'Bahasa Kalbu', '  ', 1, '2019-03-23 01:44:01');
-/*!40000 ALTER TABLE `bahasa` ENABLE KEYS */;
-
--- Dumping structure for table elibrary.berita
-CREATE TABLE IF NOT EXISTS `berita` (
-  `id_berita` int(11) NOT NULL AUTO_INCREMENT,
-  `id_user` int(11) NOT NULL,
-  `slug_berita` varchar(255) CHARACTER SET utf8 NOT NULL,
-  `judul_berita` varchar(255) CHARACTER SET utf8 NOT NULL,
-  `isi` text CHARACTER SET utf8 NOT NULL,
-  `gambar` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
-  `status_berita` enum('Publish','Draft','','') CHARACTER SET utf8 NOT NULL,
-  `jenis_berita` enum('Berita','Slide','','') NOT NULL,
-  `tanggal` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id_berita`),
-  UNIQUE KEY `judul_berita` (`judul_berita`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
-
--- Dumping data for table elibrary.berita: ~5 rows (approximately)
-/*!40000 ALTER TABLE `berita` DISABLE KEYS */;
-INSERT INTO `berita` (`id_berita`, `id_user`, `slug_berita`, `judul_berita`, `isi`, `gambar`, `status_berita`, `jenis_berita`, `tanggal`) VALUES
-	(1, 4, 'iwak-e-nyebur-sumur-tandas', 'iwak e nyebur sumur tandas', '<p>asdasdasd asd</p>', 'kandang-ternak-kenari-siste5.jpg', 'Publish', 'Berita', '2019-03-25 20:24:14'),
-	(4, 4, 'hard-work', 'Hard Work', '<p>Hard work is but one of the ways you can achieve your goals. For those of us who aren&rsquo;t inordinately&nbsp;<a href="https://www.primermagazine.com/2011/learn/the-only-4-reasons-why-your-peers-are-more-successful-than-you">wealthy, smart, or lucky</a>, it&rsquo;s the only way. While each person&rsquo;s path to success will be unique, the anatomy of the hard work that they do often looks very similar.</p>', 'b1.jpg', 'Publish', 'Slide', '2019-03-25 20:31:57'),
-	(5, 4, 'study', 'Study', '<p><span class="ind">The devotion of time and attention to gaining knowledge of an academic subject, especially by means of books.</span></p>', 'b2.jpg', 'Publish', 'Slide', '2019-03-25 20:30:29'),
-	(6, 4, 'class', 'Class', '<p>Learning is the process of acquiring new, or modifying existing, <span style="text-decoration: underline;"><a title="Knowledge" href="https://en.wikipedia.org/wiki/Knowledge">knowledge</a></span>, <span style="text-decoration: underline;"><a title="Behavior" href="https://en.wikipedia.org/wiki/Behavior">behaviors</a>, </span><a title="Skill" href="https://en.wikipedia.org/wiki/Skill">skills</a>, <a class="mw-redirect" title="Value (personal and cultural)" href="https://en.wikipedia.org/wiki/Value_(personal_and_cultural)">values</a>, or <a title="Preference" href="https://en.wikipedia.org/wiki/Preference">preferences</a>.<sup id="cite_ref-1" class="reference"></sup></p>', 'b3.jpg', 'Publish', 'Slide', '2019-03-25 20:34:21'),
-	(7, 4, 'sayur-kol', 'Sayur Kol', '<p>Makan daging teman dengan sayur kol</p>', 'Sabyan_Gambus_Nissa_Sabyan_1548167665.jpeg', 'Publish', 'Berita', '2019-03-25 23:41:34');
-/*!40000 ALTER TABLE `berita` ENABLE KEYS */;
 
 -- Dumping structure for table elibrary.buku
 CREATE TABLE IF NOT EXISTS `buku` (
   `id_buku` int(11) NOT NULL AUTO_INCREMENT,
-  `id_user` int(11) NOT NULL,
   `id_jenis` int(11) NOT NULL,
-  `id_bahasa` int(11) NOT NULL,
   `judul_buku` varchar(255) NOT NULL,
   `penulis_buku` varchar(255) NOT NULL,
   `subjek_buku` varchar(255) DEFAULT NULL,
-  `letak_buku` varchar(50) DEFAULT NULL,
+  `Serve_for` varchar(50) DEFAULT NULL,
   `kode_buku` varchar(50) DEFAULT NULL,
-  `kolasi` int(11) DEFAULT NULL,
   `penerbit` varchar(255) DEFAULT NULL,
   `tahun_terbit` year(4) DEFAULT NULL,
   `no_seri` varchar(50) DEFAULT NULL,
@@ -109,22 +56,21 @@ CREATE TABLE IF NOT EXISTS `buku` (
 
 -- Dumping data for table elibrary.buku: ~8 rows (approximately)
 /*!40000 ALTER TABLE `buku` DISABLE KEYS */;
-INSERT INTO `buku` (`id_buku`, `id_user`, `id_jenis`, `id_bahasa`, `judul_buku`, `penulis_buku`, `subjek_buku`, `letak_buku`, `kode_buku`, `kolasi`, `penerbit`, `tahun_terbit`, `no_seri`, `status_buku`, `ringkasan`, `cover_buku`, `jumlah_buku`, `tanggal_entri`, `tanggal`) VALUES
-	(5, 4, 3, 1, 'Ilmu Pengetahuan Sosial', 'Nur Wahyu Rochmadi', 'Sekolah Menengah Kejuruan', '', 'IPSJD1', 11, 'Buku Sekolah Elektronik (BSE)', '2004', '123xr3', 'Publish', 'Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.\r\n\r\nA small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.', 'ips.jpg', 20, '2019-03-22 20:14:19', '2019-03-28 15:58:55'),
-	(6, 4, 3, 1, 'Dasar Kewirausahaan', 'Ir. Hendro', '', '', '', 0, '', '0000', '', 'Publish', '  ', '20170212035154.jpg', 0, '2019-03-23 14:33:09', '2019-03-23 20:33:09'),
-	(7, 4, 3, 1, 'PHP Modul', 'Teguh Wahyono', '', '', '', 0, '', '0000', '', 'Publish', '  ', '20170212145310.jpg', 0, '2019-03-25 18:10:31', '2019-03-26 00:10:31'),
-	(8, 4, 3, 1, 'Pengantar Teknologi Informasi', 'Eddy Sutanta', '', '', '', 0, '', '0000', '', 'Publish', '  ', '20170209044244.jpg', 0, '2019-03-25 18:11:29', '2019-03-26 00:11:29'),
-	(9, 4, 3, 1, 'Kamus Istilah Internet', 'wang cun', '', '', '', 0, '', '0000', '', 'Publish', '  ', '20170212080423.jpg', 0, '2019-03-25 18:12:13', '2019-03-26 00:12:13'),
-	(10, 4, 3, 1, 'Kamus Matematika', 'ario', '', '', '', 0, '', '0000', '', 'Publish', '  ', '20170207102926.jpg', 0, '2019-03-25 18:12:56', '2019-03-26 00:12:56'),
-	(11, 4, 3, 1, 'E-Learning', 'mario', '', '', '', 0, '', '0000', '', 'Publish', '  ', '20170209050821.jpg', 0, '2019-03-25 18:13:39', '2019-03-26 00:13:39'),
-	(12, 4, 3, 1, 'Algoritma C++', 'niawarti', '', '', '', 0, '', '0000', '', 'Publish', '  ', '20170209045014.jpg', 0, '2019-03-25 18:14:22', '2019-03-26 00:14:22');
+INSERT INTO `buku` (`id_buku`, `id_jenis`, `judul_buku`, `penulis_buku`, `subjek_buku`, `Serve_for`, `kode_buku`, `penerbit`, `tahun_terbit`, `no_seri`, `status_buku`, `ringkasan`, `cover_buku`, `jumlah_buku`, `tanggal_entri`, `tanggal`) VALUES
+	(5, 3, 'Ilmu Pengetahuan Sosial', 'Nur Wahyu Rochmadi', 'Sekolah Menengah Kejuruan', 'Available', 'IPSJD1', 'Buku Sekolah Elektronik (BSE)', '2004', '123xr3', 'Publish', 'Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.\r\n\r\nA small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.', 'ips.jpg', 20, '2019-03-22 20:14:19', '2019-05-17 01:30:41'),
+	(6, 3, 'Dasar Kewirausahaan', 'Ir. Hendro', '', 'Available', '', '', '0000', '', 'Publish', '  ', '20170212035154.jpg', 0, '2019-03-23 14:33:09', '2019-05-17 01:30:43'),
+	(7, 3, 'PHP Modul', 'Teguh Wahyono', '', 'Available', '', '', '0000', '', 'Publish', '  ', '20170212145310.jpg', 0, '2019-03-25 18:10:31', '2019-05-17 01:30:44'),
+	(8, 3, 'Pengantar Teknologi Informasi', 'Eddy Sutanta', '', 'Available', '', '', '0000', '', 'Publish', '  ', '20170209044244.jpg', 0, '2019-03-25 18:11:29', '2019-05-17 01:30:44'),
+	(9, 3, 'Kamus Istilah Internet', 'wang cun', '', 'Available', '', '', '0000', '', 'Publish', '  ', '20170212080423.jpg', 0, '2019-03-25 18:12:13', '2019-05-17 01:30:46'),
+	(10, 3, 'Kamus Matematika', 'ario', '', 'Available', '', '', '0000', '', 'Publish', '  ', '20170207102926.jpg', 0, '2019-03-25 18:12:56', '2019-05-17 01:30:47'),
+	(11, 3, 'E-Learning', 'mario', '', 'Available', '', '', '0000', '', 'Publish', '  ', '20170209050821.jpg', 0, '2019-03-25 18:13:39', '2019-05-17 01:30:47'),
+	(12, 3, 'Algoritma C++', 'niawarti', '', 'Available', '', '', '0000', '', 'Publish', '  ', '20170209045014.jpg', 0, '2019-03-25 18:14:22', '2019-05-17 01:30:50');
 /*!40000 ALTER TABLE `buku` ENABLE KEYS */;
 
 -- Dumping structure for table elibrary.file
 CREATE TABLE IF NOT EXISTS `file` (
   `id_file` int(11) NOT NULL AUTO_INCREMENT,
   `id_buku` int(11) NOT NULL,
-  `id_user` int(11) NOT NULL,
   `judul_file` varchar(255) NOT NULL,
   `nama_file` varchar(255) NOT NULL,
   `keterangan` text,
@@ -135,9 +81,9 @@ CREATE TABLE IF NOT EXISTS `file` (
 
 -- Dumping data for table elibrary.file: ~2 rows (approximately)
 /*!40000 ALTER TABLE `file` DISABLE KEYS */;
-INSERT INTO `file` (`id_file`, `id_buku`, `id_user`, `judul_file`, `nama_file`, `keterangan`, `urutan`, `tanggal`) VALUES
-	(34, 5, 4, 'bab 3', 'agilinilucu.docx', 'qweasd', 2, '2019-03-25 00:44:30'),
-	(35, 5, 4, 'bab 1', 'Tata_Tertib_Kos_Ibu_Said.pdf', 'asdasd', 1, '2019-03-29 23:58:24');
+INSERT INTO `file` (`id_file`, `id_buku`, `judul_file`, `nama_file`, `keterangan`, `urutan`, `tanggal`) VALUES
+	(34, 5, 'bab 3', 'agilinilucu.docx', 'qweasd', 2, '2019-03-25 00:44:30'),
+	(35, 5, 'bab 1', 'Tata_Tertib_Kos_Ibu_Said.pdf', 'asdasd', 1, '2019-03-29 23:58:24');
 /*!40000 ALTER TABLE `file` ENABLE KEYS */;
 
 -- Dumping structure for table elibrary.jenis
@@ -280,16 +226,19 @@ INSERT INTO `tbl_setting` (`Variable`, `Value`) VALUES
 -- Dumping structure for table elibrary.tbl_user
 CREATE TABLE IF NOT EXISTS `tbl_user` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `id_anggota` int(11) NOT NULL,
   `Username` varchar(15) NOT NULL,
   `Password` text NOT NULL,
   `Email` varchar(100) NOT NULL,
+  `nama` varchar(50) DEFAULT NULL,
+  `akses_level` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`ID`)
 ) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 -- Dumping data for table elibrary.tbl_user: 1 rows
 /*!40000 ALTER TABLE `tbl_user` DISABLE KEYS */;
-INSERT INTO `tbl_user` (`ID`, `Username`, `Password`, `Email`) VALUES
-	(1, 'admin', 'a74ac0a61316e8e74256de730cd32a78eac4653f10b485cbb452612367b61fcf', 'developer.solodroid@gmail.com');
+INSERT INTO `tbl_user` (`ID`, `id_anggota`, `Username`, `Password`, `Email`, `nama`, `akses_level`) VALUES
+	(1, 0, 'admin', 'a74ac0a61316e8e74256de730cd32a78eac4653f10b485cbb452612367b61fcf', 'developer.solodroid@gmail.com', NULL, NULL);
 /*!40000 ALTER TABLE `tbl_user` ENABLE KEYS */;
 
 -- Dumping structure for table elibrary.usulan
